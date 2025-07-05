dd4edd7e21b28d14f64fbaf8de26cc1961c5adb8
tanawin <tanawin1701d@outlook.com>
2025-07-05 10:02:14 +0200

    update parameter

--- srcHdl/m_axi_read.v
+++ srcHdl/m_axi_read.v
@@ -14,7 +14,7 @@ module m_axi_read #(
     parameter BANK0_STATUS_WIDTH  = 4,
     parameter BANK0_CNT_WIDTH     = BANK1_INDEX_WIDTH, /// the counter for the sequencer
 
-    parameter DMA_INIT_TASK_CNT   = 6, //// (baseAddr0 + size0) + (baseAddr1 + size1)
+    parameter DMA_INIT_TASK_CNT   = 8, //// (baseAddr0 + size0) + (baseAddr1 + size1)
     parameter DMA_EXEC_TASK_CNT   = 1
 )(
 
