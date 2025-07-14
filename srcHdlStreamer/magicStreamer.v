d53cbe7193df1426ea8c03e612e2fa188441cdc4
tanawin <tanawin1701d@outlook.com>
2025-07-14 08:23:34 +0200

    fix mis interrupt signal and system and upgrade memory inference

--- srcHdlStreamer/magicStreamer.v
+++ srcHdlStreamer/magicStreamer.v
@@ -29,7 +29,12 @@ module my_axis_slave #
     input wire loadInit,
 
     // store complete connect it to mgsFinExec
-    output wire finStore
+    output wire finStore,
+
+    // out put wire for debugging
+    output wire [STATE_BIT_WIDTH-1:0] dbg_state,
+    output wire [STORAGE_IDX_WIDTH-1:0] dbg_amt_store_bytes,
+    output wire [STORAGE_IDX_WIDTH-1:0] dbg_amt_load_bytes
 
 );
 
@@ -49,6 +54,8 @@ reg[STORAGE_IDX_WIDTH-1: 0] amt_store_bytes; ///// store to this block
 reg[STORAGE_IDX_WIDTH-1: 0] amt_load_bytes;  ///// load to this block
 reg storeIntr;
 
+wire[STORAGE_IDX_WIDTH-1: 0] pooled_addr = (state == STATUS_STORE) ? amt_store_bytes : amt_load_bytes;
+
 /////////////////////////////////////
 ////// axi signal assign ////////////
 /////////////////////////////////////
@@ -56,42 +63,50 @@ reg storeIntr;
 ///////// store
 assign S_AXI_TREADY = (state == STATUS_STORE) && S_AXI_TVALID;
 ///////// load
-assign M_AXI_TDATA   = mainMem[amt_load_bytes];
+assign M_AXI_TDATA   = mainMem[pooled_addr];
 assign M_AXI_TKEEP   = 4'b1111;
 assign M_AXI_TVALID  = (state == STATUS_LOAD);
 assign M_AXI_TLAST   = (state == STATUS_LOAD) && (amt_load_bytes == (amt_store_bytes-1));
+///////// interrupt signal
+assign finStore = storeIntr;
+/////////// debug signal
+assign dbg_state           = state;
+assign dbg_amt_store_bytes = amt_store_bytes;
+assign dbg_amt_load_bytes  = amt_load_bytes;
 
-///// control system
-
+/////////////////////////////////////
+////// control system    ////////////
+/////////////////////////////////////
 always @(posedge clk, negedge reset) begin
     
     if (~reset) begin
+        state           <= STATUS_IDLE;
         amt_store_bytes <= 0;
-        amt_load_bytes <= 0;
-        storeIntr <= 0;
+        amt_load_bytes  <= 0;
+        storeIntr       <= 0;
     end else begin
         case (state) 
             STATUS_IDLE    : begin 
                     if (storeReset) begin
                         amt_store_bytes <= 0;
-                        storeIntr <= 0;
+                        storeIntr       <= 0;
                     end else if (loadReset) begin
                         amt_load_bytes <= 0;
-                        storeIntr <= 0;
+                        storeIntr      <= 0;
                     end else if (storeInit) begin
                         state <= STATUS_STORE;
                     end else if (loadInit) begin
                         state <= STATUS_LOAD;
                     end
             end
-            //////////// case store data to the mainMemory
+            //////////// case store data to the internal memory
             STATUS_STORE    : begin 
                 if (S_AXI_TVALID)begin //// we are sure that ready will send this time              
-                    mainMem[amt_store_bytes] <= S_AXI_TDATA;
+                    mainMem[pooled_addr] <= S_AXI_TDATA;
                     amt_store_bytes <= amt_store_bytes + 1;
                     if (S_AXI_TLAST)begin
                         storeIntr <= 1;
-                        state <= STATUS_IDLE;
+                        state     <= STATUS_IDLE;
                     end
                 end
             end
@@ -110,9 +125,5 @@ always @(posedge clk, negedge reset) begin
     end 
 
 end
-
-
-
-
 
 endmodule
