d97356fcbe52a69961ce16f0cb68dd7638d0a512
tanawin <tanawin1701d@outlook.com>
2025-08-01 18:22:01 +0200

    update driver for magic streamer debugger

---
+++ srcDriver/dfx4ml/magicStreamDbg.py
@@ -0,0 +1,70 @@
+# import the library
+from pynq import Overlay     # import the overlay
+from pynq import allocate    # import for CMA (contingeous memory allocation)
+from pynq import DefaultIP   # import the ip connector library for extension
+import numpy as np
+import os
+import subprocess
+import re
+
+class MagicStreamDbg:
+
+    
+    LOAD_ADDR = 0x8
+    STORE_STATE_ADDR = 0x0
+
+
+    def __init__(self, debugIps, widthSizes):
+
+        self.dbIps = debugIps
+        self.dbBws = widthSizes
+
+        if len(debugIps) != len(widthSizes):
+            raise Exception("There are mismatch metadata of dubugIps and widthSizes")
+
+    def createMask(length):
+        return (1 << length) -1
+
+    def cvtRawToStoreVal(self, streamIdx, rawData):
+        return rawData & self.createMask(self.dbBws[streamIdx]+ 1)
+    
+    def cvtRawToStateVal(self, streamIdx, rawData):
+        return rawData >> (self.dbBws[streamIdx] + 1)
+    
+    def getMaxUsage(self, streamIdx):
+        width = self.dbBws[streamIdx]
+
+        return 1 << width
+
+
+    ##############################################
+    ############# get data function ##############
+    ##############################################
+    def getLoadAmtValue(self, streamIdx):
+        return self.dbIps[streamIdx].read(self.LOAD_ADDR)
+    
+    def getStoreAmtValue(self, streamIdx):
+        rawValue =  self.dbIps[streamIdx].read(self.STORE_STATE_ADDR)
+        return self.cvtRawToStoreVal(rawValue)
+    
+    def getStateValue(self, streamIdx):
+        rawValue = self.dbIps[streamIdx].read(self.STORE_STATE_ADDR)
+        stateValue = self.cvtRawToStateVal(streamIdx, rawValue)
+
+        stateMap = ["STATUS_IDLE","STATUS_STORE","STATUS_LOAD"]
+
+        return stateMap[stateValue]
+    
+
+    ##############################################
+    ############# print Dbg function #############
+    ##############################################
+
+    def printDebugAll(self):
+        print("magic streamer debugger")
+        for idx in range(len(self.dbIps)):
+            print(f"-------- STREAM {idx}----------")
+            print(f"state: {self.getStateValue(idx)}")
+            print(f"amtLoad: {str(self.getLoadAmtValue(idx))} bytes/ {self.getMaxUsage(idx)} bytes")
+            print(f"amtStore: {str(self.getStoreAmtValue(idx))} bytes/ {self.getMaxUsage(idx)} bytes")
+        print("----------------------------------")
